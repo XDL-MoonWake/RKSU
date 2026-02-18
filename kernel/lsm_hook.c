8c9857ac654acd0969c9a911fd79c81e09c701f6
RainyXeon <rainyxeon@gmail.com>
2026-02-18 16:36:32 +0700

    fix: compiler error from unused files

--- kernel/lsm_hook.c
+++ kernel/lsm_hook.c
@@ -8,12 +8,10 @@
 
 #include "allowlist.h"
 #include "arch.h"
-#include "kp_hook.h"
 #include "ksu.h"
 #include "klog.h" // IWYU pragma: keep
 #include "ksud.h"
 #include "kernel_compat.h"
-#include "kp_util.h"
 #include "supercalls.h"
 #include "sucompat.h"
 #include "setuid_hook.h"
